bef76385541f5c5ff2e85fbd1d6173fa4a16fcff
ismay <ismay@ismaywolff.nl>
2021-05-28 13:00:41 +0200

    Add storybook configuration

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+const applyMocks = (config) => {
+  config.resolve.alias["next/router"] = require.resolve(
+    "./mocks/next-router.js"
+  );
+
+  return config;
+};
+
+module.exports = {
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  core: {
+    builder: "webpack5",
+  },
+  stories: ["../components/**/*.stories.js"],
+  webpackFinal: applyMocks,
+};
---
+++ .storybook/mocks/next-router.js
@@ -0,0 +1,3 @@
+module.exports = {
+  useRouter: () => ({ pathname: "" }),
+};

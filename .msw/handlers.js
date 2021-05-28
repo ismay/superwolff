d29f03f7fdf6cec7f2a13dd68dfa923b32951adf
ismay <ismay@ismaywolff.nl>
2021-05-28 13:03:59 +0200

    Add api mocking with msw

---
+++ .msw/handlers.js
@@ -0,0 +1,35 @@
+import { graphql } from "msw";
+import { one, three, two } from "./mocks/works";
+
+const works = [one, two, three];
+
+const handlers = [
+  graphql.query("getWorks", (req, res, ctx) =>
+    res(
+      ctx.data({
+        works: [one, two, three],
+      })
+    )
+  ),
+
+  graphql.query("getWorkSlugs", (req, res, ctx) =>
+    res(
+      ctx.data({
+        works,
+      })
+    )
+  ),
+
+  graphql.query("getWork", (req, res, ctx) => {
+    const { slug } = req.variables;
+    const work = works.find((w) => w.slug === slug);
+
+    return res(
+      ctx.data({
+        work,
+      })
+    );
+  }),
+];
+
+export default handlers;
---
+++ .msw/mocks/works.js
@@ -0,0 +1,104 @@
+export const one = {
+  dimensions: "100 x 100 cm",
+  id: "1",
+  images: [
+    {
+      height: 927,
+      id: "1",
+      url: "/_images/927x1500.jpg",
+      width: 1500,
+    },
+    {
+      height: 927,
+      id: "2",
+      url: "/_images/927x1500.jpg",
+      width: 1500,
+    },
+    {
+      height: 927,
+      id: "3",
+      url: "/_images/927x1500.jpg",
+      width: 1500,
+    },
+  ],
+  medium: "Inkjet print",
+  releasedAt: "2020-01-01",
+  slug: "one",
+  thumbnail: {
+    height: 927,
+    url: "/_images/927x1500.jpg",
+    width: 1500,
+  },
+  title: "One",
+  videos: [],
+};
+
+export const two = {
+  dimensions: "100 x 100 cm",
+  id: "2",
+  images: [
+    {
+      height: 1500,
+      id: "1",
+      url: "/_images/1500x927.jpg",
+      width: 927,
+    },
+    {
+      height: 1500,
+      id: "2",
+      url: "/_images/1500x927.jpg",
+      width: 927,
+    },
+    {
+      height: 1500,
+      id: "3",
+      url: "/_images/1500x927.jpg",
+      width: 927,
+    },
+  ],
+  medium: "Inkjet print",
+  releasedAt: "2020-02-02",
+  slug: "two",
+  thumbnail: {
+    height: 927,
+    url: "/_images/927x1500.jpg",
+    width: 1500,
+  },
+  title: "Two",
+  videos: [],
+};
+
+export const three = {
+  dimensions: "100 x 100 cm",
+  id: "3",
+  images: [
+    {
+      height: 1500,
+      id: "1",
+      url: "/_images/1500x1500.jpg",
+      width: 1500,
+    },
+    {
+      height: 1500,
+      id: "2",
+      url: "/_images/1500x1500.jpg",
+      width: 1500,
+    },
+    {
+      height: 1500,
+      id: "3",
+      url: "/_images/1500x1500.jpg",
+      width: 1500,
+    },
+  ],
+  medium: "Inkjet print",
+  releasedAt: "2020-03-03",
+  slug: "three",
+  thumbnail: {
+    height: 927,
+    url: "/_images/927x1500.jpg",
+    width: 1500,
+  },
+  title: "Three",
+  videos: [],
+};

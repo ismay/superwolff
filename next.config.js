72cde357cefff359030c6800acba0483b57d4974
ismay <ismay@ismaywolff.nl>
2021-06-19 10:01:33 +0200

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-module.exports = {
+/* eslint-disable import/no-extraneous-dependencies */
+
+const bundleAnalyzer = require("@next/bundle-analyzer");
+
+const config = {
   images: {
     domains: ["media.graphcms.com"],
   },
   reactStrictMode: true,
 };
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE_BUNDLE === "enabled",
+});
+
+module.exports = withBundleAnalyzer(config);

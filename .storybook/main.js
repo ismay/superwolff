d6454334e633d9d27de72f7c4c7f4006ee16cd62
ismay <ismay@ismaywolff.nl>
2021-06-23 18:35:36 +0200

    Update storybook to latest

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,9 +8,6 @@ const applyMocks = (config) => {
 
 module.exports = {
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  core: {
-    builder: "webpack5",
-  },
   stories: ["../components/**/*.stories.js"],
   webpackFinal: applyMocks,
 };

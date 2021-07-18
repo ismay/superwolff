cf155ef0d285b69893ea193a3523930bfb80a4ea
ismay <ismay@ismaywolff.nl>
2021-07-18 10:26:50 +0200

    Use mdx for about page

--- pages/about.js
+++ pages/about.js
@@ -1,41 +1,43 @@
-import Head from "next/head";
+/* eslint-disable react/jsx-props-no-spreading, react/forbid-prop-types */
 
-export default function About() {
+import { gql } from "graphql-request";
+import { MDXRemote } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+import Head from "next/head";
+import T from "prop-types";
+import client from "../client";
+
+export default function About({ source }) {
   return (
     <>
       <Head>
         <title>About - Superwolff</title>
       </Head>
-      <h2>About</h2>
-      <p>
-        Superwolff was born in 1983 in Heerde, the Netherlands. He graduated
-        with a BA in Fine and Studio Arts from Academy Minerva of Groningen.
-      </p>
-      <p>
-        His work has been exhibited internationally, including the LA Art Show
-        (Los Angeles, US), Artolive Young Talent (Amsterdam, NL) and the EEA
-        Decennial at Flutgraben (Berlin, DE). It has been covered in several
-        publications, most notably Volkskrant Magazine and NRC Next.
-      </p>
-      <p>
-        He lives and works in Groningen, the Netherlands. His CV can be
-        downloaded{" "}
-        <a href="/pdf/cv-superwolff.pdf" title="View CV">
-          here
-        </a>{" "}
-        and he can be contacted via{" "}
-        <a href="mailto:superwolff@superwolff.nl" title="Email Superwolff">
-          email
-        </a>
-        . Prints of his work can be bought through{" "}
-        <a
-          href="https://theartling.com/en/print-artists/superwolff"
-          title="Buy prints at The Artling"
-        >
-          The Artling
-        </a>
-        .
-      </p>
+      <MDXRemote {...source} />
     </>
   );
+}
+
+About.propTypes = {
+  source: T.object.isRequired,
+};
+
+export async function getStaticProps() {
+  const { page } = await client.request(
+    gql`
+      query getPage($title: String!) {
+        page(where: { title: $title }) {
+          mdx
+        }
+      }
+    `,
+    { title: "About" }
+  );
+
+  const source = await serialize(page.mdx);
+
+  return {
+    props: { source },
+    revalidate: 3600,
+  };
 }

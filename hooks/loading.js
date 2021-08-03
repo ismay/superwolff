a1ae13e8cf69cfc1fdda96dbcdc8a280fb7448a2
ismay <ismay@ismaywolff.nl>
2021-08-03 10:36:15 +0200

    Add panelbear

---
+++ hooks/loading.js
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function useLoading() {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const handleRouteChangeStart = () => {
+      setIsLoading(true);
+    };
+    const handleRouteChangeEnd = () => {
+      setIsLoading(false);
+    };
+
+    router.events.on("routeChangeStart", handleRouteChangeStart);
+    router.events.on("routeChangeComplete", handleRouteChangeEnd);
+    router.events.on("routeChangeError", handleRouteChangeEnd);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeStart);
+      router.events.off("routeChangeComplete", handleRouteChangeEnd);
+      router.events.off("routeChangeError", handleRouteChangeEnd);
+    };
+  });
+
+  return isLoading;
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,21 @@
 /* eslint-disable react/prop-types, react/jsx-props-no-spreading, global-require */
 
 import "../styles/globals.css";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import LoadingIndicator from "../components/loading-indicator";
 import Main from "../components/main";
 import Page from "../components/page";
+import useLoading from "../hooks/loading";
+import usePanelbear from "../hooks/panelbear";
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
   require("../.msw");
 }
 
 export default function CustomApp({ Component, pageProps }) {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const handleRouteChangeStart = () => {
-      setIsLoading(true);
-    };
-    const handleRouteChangeEnd = () => {
-      setIsLoading(false);
-    };
-
-    router.events.on("routeChangeStart", handleRouteChangeStart);
-    router.events.on("routeChangeComplete", handleRouteChangeEnd);
-    router.events.on("routeChangeError", handleRouteChangeEnd);
-
-    return () => {
-      router.events.off("routeChangeStart", handleRouteChangeStart);
-      router.events.off("routeChangeComplete", handleRouteChangeEnd);
-      router.events.off("routeChangeError", handleRouteChangeEnd);
-    };
-  });
+  usePanelbear(process.env.NEXT_PUBLIC_PANELBEAR_SITE_ID);
+  const isLoading = useLoading();
 
   return (
     <Page>

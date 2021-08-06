4b1f5017f994cdb4e88778cf63a1ec107c291028
ismay <ismay@ismaywolff.nl>
2021-08-06 10:26:02 +0200

    Update browser settings for percy

--- .msw/mocks/pages.js
+++ .msw/mocks/pages.js
@@ -1,7 +1,12 @@
-export const about = {
-  mdx: `## About
+const mdx = `
+## About
 
-    This is mock content for the about page.
-  `,
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam imperdiet volutpat eros, in iaculis quam varius nec. Suspendisse a egestas magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Donec et pretium mauris. Etiam vitae nisl pulvinar, congue massa nec, mattis mi. Aliquam id augue fermentum, egestas purus in, bibendum dui. Nunc ac laoreet neque. Nam porta sodales scelerisque.
+`;
+
+export const about = {
+  mdx,
   title: "About",
 };

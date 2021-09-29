d1d62bfe227779bb3a6e33d9482edb9a12cac0b6
ismay <ismay@ismaywolff.nl>
2021-09-29 22:10:22 +0200

    Remove panelbear

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types, react/jsx-props-no-spreading, global-require */
 
 import "../styles/globals.css";
-import { usePanelbear } from "@panelbear/panelbear-nextjs";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import LoadingIndicator from "../components/loading-indicator";
@@ -14,7 +13,6 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
 }
 
 export default function CustomApp({ Component, pageProps }) {
-  usePanelbear(process.env.NEXT_PUBLIC_PANELBEAR_SITE_ID);
   const isLoading = useLoading();
 
   return (
